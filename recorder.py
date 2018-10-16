2b48dba0a85af8b2c6e11c2a9d336bbe785e017c
Florian Reimair <office@florianreimair.at>
2018-10-16 17:11:13 +0200

    write calculated distance to logbook

--- recorder.py
+++ recorder.py
@@ -1,13 +1,15 @@
 import asyncio
 import serial
+import geopy.distance
 
 class Recorder():
 
     def __init__(self):
-        self.distance = 31
+        self.distance = 0
         self.interval = 3
         self.serial = serial.Serial("/dev/ttyUSB0", 4800)
         self.old_position = (0, 0)
+        self.tack = 0
 
     async def update(self):
         while True:
@@ -21,9 +23,11 @@ class Recorder():
             print(new_position)
 
             if 0 != self.old_position[0] and 0 != self.old_position[1]:
+                #calculate distance
+                self.tack = geopy.distance.vincenty(self.old_position, new_position).nm
 
                 #update self.distance
-                self.distance = self.distance + 1
+                self.distance = self.distance + self.tack
                 print("distance updated to " + str(self.distance))
 
             #update reference position
@@ -31,4 +35,4 @@ class Recorder():
             await asyncio.sleep(self.interval)
 
     def getDistanceTravelled(self):
-        return self.distance;
+        return float("{0:.2f}".format(round(self.distance,2)))
