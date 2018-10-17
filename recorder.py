9085b9a9c0e3b819e0a66303c80dc661b22ee5ed
Florian Reimair <office@florianreimair.at>
2018-10-17 08:32:14 +0200

    use gps timestamps for speed calculation

--- recorder.py
+++ recorder.py
@@ -6,9 +6,11 @@ class Recorder():
 
     def __init__(self):
         self.distance = 0
-        self.interval = 3
+        self.interval = 20
         self.serial = serial.Serial("/dev/ttyUSB0", 4800)
         self.old_position = (0, 0)
+        self.old_timestamp = 0
+        self.tackspeed = 0
         self.tack = 0
 
     async def update(self):
@@ -22,9 +24,13 @@ class Recorder():
             #write position to file (gpx?)
             print(new_position)
 
-            if 0 != self.old_position[0] and 0 != self.old_position[1]:
+            if 0 != self.old_position[0] and 0 != self.old_position[1] and 0 != self.old_timestamp:
                 #calculate distance
                 self.tack = geopy.distance.vincenty(self.old_position, new_position).nm
+
+                #calculate tack speed
+                self.tackspeed = self.tack / (float(line[1]) - self.old_timestamp) * 3600
+                print("tack speed has been " + str(self.getCurrentSpeed()))
 
                 #update self.distance
                 self.distance = self.distance + self.tack
@@ -32,10 +38,11 @@ class Recorder():
 
             #update reference position
             self.old_position = new_position
+            self.old_timestamp = float(line[1])
             await asyncio.sleep(self.interval)
 
     def getDistanceTravelled(self):
         return float("{0:.2f}".format(round(self.distance,2)))
 
     def getCurrentSpeed(self):
-        return float("{0:.2f}".format(round(self.tack/(self.interval/3600),2)))
+        return float("{0:.2f}".format(round(self.tackspeed,2)))
