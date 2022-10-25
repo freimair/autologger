25d6ee98ae8e61d296e102f9974c63b71bc70b61
Florian Reimair <office@florianreimair.at>
2022-10-25 11:31:36 +0200

    Move message templates to another file

---
+++ statics/js/message_templates.js
@@ -0,0 +1,32 @@
+let safetyBriefing = `<h3>Befehlskette</h3>
+<input type="checkbox" checked> im Zweifel alles melden
+
+<h3>Bewegung an Bord</h3>
+<br /><input type="checkbox"> Außerhalb von Cockpit nur auf Anweisung
+<br /><input type="checkbox"> Schuhe
+<br /><input type="checkbox"> nicht laufen, nicht springen
+<br /><input type="checkbox"> Ordnung halten
+<br /><input type="checkbox"> alles Schwimmer?
+
+<h3>Gefahren durch</h3>
+<br /><input type="checkbox"> Großbaum
+<br /><input type="checkbox"> Winschen
+<br /><input type="checkbox"> Klampen/Klemmen
+<br /><input type="checkbox"> Leinen
+<br /><input type="checkbox"> Kollisionsvermeidung
+<br /><input type="checkbox"> Niedergang
+<br /><input type="checkbox"> Stolperfallen
+<br /><input type="checkbox"> Nächtliches Strullern
+<br /><input type="checkbox"> SmartPhone
+
+<h3>Maßnahmen</h3>
+<br /><input type="checkbox"> Rettungswesten und Lifebelt
+<br /><input type="checkbox"> Man over board: MOB-Taste drücken, Alarm, hinzeigen
+<br /><input type="checkbox"> Feuerlöscher/-decke
+<br /><input type="checkbox"> Distress-Taste
+<br /><input type="checkbox"> Verbandskasten
+<br /><input type="checkbox"> Grab-Bag
+<br /><input type="checkbox"> Motor Bedienung
+`
+
+let note = `<textarea></textarea>`
