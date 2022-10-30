b5f9630d5751751b465c7d07db29062e9ad5dc94
Florian Reimair <office@florianreimair.at>
2022-10-30 14:12:52 +0100

    Limit graph history length to fixed value

--- statics/js/chart.js
+++ statics/js/chart.js
@@ -143,22 +143,43 @@ class Charts {
     if (incoming.SoG) {
       this.chart_SoG.data.labels.push(incoming.DateTime);
       this.chart_SoG.data.datasets[0].data.push(incoming.SoG);
+
+      if (150 < this.chart_SoG.data.labels.length) {
+        this.chart_SoG.data.labels.shift();
+        this.chart_SoG.data.datasets[0].data.shift();
+      }
+
       this.chart_SoG.update();
     }
     if (incoming.AirTemperature && incoming.AirPressure) {
       this.chart_weather.data.labels.push(incoming.DateTime);
       this.chart_weather.data.datasets[0].data.push(incoming.AirTemperature);
       this.chart_weather.data.datasets[1].data.push(incoming.AirPressure);
+
+      if (150 < this.chart_weather.data.labels.length) {
+        this.chart_weather.data.labels.shift();
+        this.chart_weather.data.datasets[0].data.shift();
+        this.chart_weather.data.datasets[1].data.shift();
+      }
+
       this.chart_weather.update();
     }
     if (incoming.Windspeed && incoming.WindAngle) {
       this.chart_wind.data.labels.push(incoming.DateTime);
       this.chart_wind.data.datasets[0].data.push(incoming.Windspeed);
       this.chart_wind.data.datasets[1].data.push(incoming.WindAngle);
+
+      if (150 < this.chart_wind.data.labels.length) {
+        this.chart_wind.data.labels.shift();
+        this.chart_wind.data.datasets[0].data.shift();
+        this.chart_wind.data.datasets[1].data.shift();
+      }
+
       this.chart_wind.update();
     }
   }
 }
+
 $(document).ready(function () {
   window.chart = new Charts();
 });
