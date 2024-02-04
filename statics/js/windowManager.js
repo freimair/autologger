99a17c610304668adb519cb30a063885dac03f72
Florian Reimair <office@florianreimair.at>
2024-02-04 12:49:43 +0100

    Encapsulate window positions

--- statics/js/windowManager.js
+++ statics/js/windowManager.js
@@ -70,6 +70,45 @@ class MobileWindowManager extends WindowManager {
 }
 
 class DesktopWindowManager extends WindowManager {
+  defaultWindowPositions = {
+    hud: {
+      position: [5, 50],
+      width: 750,
+      height: 150,
+      show: true
+    },
+    map: {
+      position: [765, 50],
+      width: 590,
+      height: 380,
+      show: true
+    },
+    logbookcontrols: {
+      position: [1365, 50],
+      width: 200,
+      height: 380,
+      show: true
+    },
+    plots: {
+      position: [765, 430],
+      width: 800,
+      height: 525,
+      show: true
+    },
+    settingsPage: {
+      position: [600, 600],
+      width: 500,
+      height: 500,
+      show: false
+    },
+    table: {
+      position: [5, 200],
+      width: 750,
+      height: 755,
+      show: true
+    }
+  };
+
   getType() {
     return DesktopWindowManager.name;
   }
@@ -77,60 +116,31 @@ class DesktopWindowManager extends WindowManager {
   register(id) {
     $('#toc-' + id.replace('#', '')).show();
     super.register(id);
-        // load from cookie
-    //cookie = decodeURIComponent(document.cookie);
 
-    let cookie = {
-      hud: {
-        position: [5, 50],
-        width: 750,
-        height: 150,
-        show: true
-      },
-      map: {
-        position: [765, 50],
-        width: 590,
-        height: 380,
-        show: true
-      },
-      logbookcontrols: {
-        position: [1365, 50],
-        width: 200,
-        height: 380,
-        show: true
-      },
-      plots: {
-        position: [765, 430],
-        width: 800,
-        height: 525,
-        show: true
-      },
-      settingsPage: {
-        position: [600, 600],
-        width: 500,
-        height: 500,
-        show: false
-      },
-      table: {
-        position: [5, 200],
-        width: 750,
-        height: 755,
-        show: true
-      }};
+    let positions = this.loadPositions();
 
-      let htmlTagWOClassifier = id.replace('#', '');
-      $("#" + htmlTagWOClassifier).dialog({
-        width: cookie[htmlTagWOClassifier]['width'],
-        height: cookie[htmlTagWOClassifier]['height'],
-      position: { my: "left top", at: "left+"+ cookie[htmlTagWOClassifier]['position'][0]+" top+" + cookie[htmlTagWOClassifier]['position'][1], of: window},
-      autoOpen: cookie[htmlTagWOClassifier]['show'],
-      close: function(event, ui) {
-          // save to cookie
-        },
-      resizeStop: function(event, ui) {
-          // save to cookie
-        }
-      });
+    let htmlTagWOClassifier = id.replace('#', '');
+    $("#" + htmlTagWOClassifier).dialog({
+      width: positions[htmlTagWOClassifier]['width'],
+      height: positions[htmlTagWOClassifier]['height'],
+    position: { my: "left top", at: "left+"+ positions[htmlTagWOClassifier]['position'][0]+" top+" + positions[htmlTagWOClassifier]['position'][1], of: window},
+    autoOpen: positions[htmlTagWOClassifier]['show'],
+    close: function(event, ui) {
+        // save to cookie
+      },
+    resizeStop: function(event, ui) {
+        // save to cookie
+      }
+    });
+  }
+
+  /**
+   * Load window positions from cookie or default
+   * 
+   * @returns window Positions as map "windowName" => ["position" => [x, y], "width" => int, "height" => int, "show" => bool];
+   */
+  loadPositions() {
+    return this.defaultWindowPositions;
   }
 
   unregister(id) {
