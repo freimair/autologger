05e8332c30ed2d4e061dce25f8fa69560afe7a58
Florian Reimair <office@florianreimair.at>
2024-02-12 12:28:06 +0100

    Only create table if it does not exist already

--- apps/logbook/database/Model.py
+++ apps/logbook/database/Model.py
@@ -5,7 +5,7 @@ class Model:
     def createTable(self) -> None:
         with Database() as cursor:
             cursor.execute("""
-                CREATE TABLE model (
+                CREATE TABLE IF NOT EXISTS model (
                     date INTEGER,
                     type INTEGER,
                     status TEXT
