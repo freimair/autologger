2771c96eb53f1fde671fb66de06cdf687208c356
Florian Reimair <office@florianreimair.at>
2024-02-17 10:26:12 +0100

    Refactor app init

--- main.py
+++ main.py
@@ -13,33 +13,43 @@ from sources.WeatherReport_meteohr import WeatherReport
 
 app = Sanic(__name__)
 
-
-@app.route('/')
-async def index(request):
-    return response.html(router.getGui())
-
 class Router:
     def __init__(self, app):
-        #self.sources=[ReplayDatasource("mocktrip1.csv", 0.1, Accumulator(app, self, "telemetry")),
-        #              ReplayDatasource("mockWeather1.csv", 4, Accumulator(app, self, "weather"))]
-        self.sources=[]
-        #windAccumulator = Accumulator(app, self, "telemetry")
-        #self.sources=[GeneratorDatasource("Windspeed", 0, 45, 1, 0.2, windAccumulator),
-        #              GeneratorDatasource("WindAngle", 0, 360, 0.2, 0.2, windAccumulator),
-        #              ReplayDatasource("20221021.logbook", 0.2, Accumulator(app, self, "telemetry"))]
-        for current in self.sources:
-            app.add_task(current.arm())
+        self.sanic_app = app
 
         self.apps=[App(app)]
+        self.sanic_app.add_route(self.getGuiRouter, "/")
 
-    def getGui(self):
-        return T("main.html").render(tools=self.apps)
+    def arm(self, sources: list):
+        for current in sources:
+            app.add_task(current.arm())
+
+    def getGuiRouter(self, request):
+        return response.html(T("main.html").render(tools=self.apps))
 
     async def incoming(self, data1, data2):
         for current in self.apps:
             await current.incoming(data1, data2)
 
 router = Router(app)
+
+@app.after_server_start
+async def after_server_start(app, loop):
+    if(app.state.is_debug):
+        windAccumulator = Accumulator(app, router, "telemetry")
+        sources=[
+                GeneratorDatasource("Windspeed", 0, 45, 1, 0.2, windAccumulator),
+                GeneratorDatasource("WindAngle", 0, 360, 0.2, 0.2, windAccumulator),
+                ReplayDatasource("20221021.logbook", 0.2, Accumulator(app, router, "telemetry")),
+                ReplayDatasource("mockWeather1.csv", 4, Accumulator(app, router, "weather")),
+            ]
+    else:
+        sources=[
+            NMEA2000(Accumulator(app, router, "telemetry")),
+            Weatherstation(Accumulator(app, router, "weather")),
+        ]
+
+    router.arm(sources)
         
 app.static("/", "./statics")
 if __name__ == "__main__":
